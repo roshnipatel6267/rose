3798e282a08e8d9747d2cd0d89e83938e5d81c5c
roshni patel <engr.roshnipatel@gmail.com>
2024-02-28 14:43:32 +0530

    added

--- modules/vm/main.tf
+++ modules/vm/main.tf
@@ -33,7 +33,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
     username  = "azureuser"
     #public_key = file("~/.ssh/id_rsa.pub")
     #public_key = file("./.ssh/id_rsa.pub")
-    public_key = file(var.ssh_public_key_path)
+    public_key = var.ssh_public_key_path != "" ? file(var.ssh_public_key_path) : null
     
   }
   os_disk {
