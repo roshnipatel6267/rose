7912591b6504658d04de9a22be02be80ceeaf33c
roshni patel <engr.roshnipatel@gmail.com>
2024-03-01 15:28:00 +0530

    added

--- modules/vm/output.tf
+++ modules/vm/output.tf
@@ -6,3 +6,6 @@ output "storage_access_key" {
   value = azurerm_storage_account.storage_account.primary_access_key
 }*/
 
+output "ssh_public_key" {
+  value = data.azurerm_ssh_public_key.public_key.openssh_key
+}
