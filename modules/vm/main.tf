5e59cc0cb61dfefa23a65dee5a1efa7ba49dadc9
roshni patel <engr.roshnipatel@gmail.com>
2024-03-01 09:15:10 +0530

    added

--- modules/vm/main.tf
+++ modules/vm/main.tf
@@ -37,10 +37,10 @@ resource "azurerm_linux_virtual_machine" "vm" {
   }*/
   admin_ssh_key {
     username  = "azureuser"
-    public_key = file(var.ssh_public_key)
+    #public_key = file(var.ssh_public_key)
     #public_key = file("${path.module}/.ssh/id_rsa.pub")
     #public_key = file("${var.ssh_public_key_path}/file")
-    #public_key = file("~/.ssh/id_rsa.pub")
+    public_key = file("~/.ssh/id_rsa.pub")
   }
   
      custom_data = <<-EOF
